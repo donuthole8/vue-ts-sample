99aace674538c17154f28e80a591b51ac2900c53
donuthole8 <muro5866@gmail.com>
2023-09-24 15:10:34 +0900

    feat:入力フォーム

--- src/components/PersonPostForm.vue
+++ src/components/PersonPostForm.vue
@@ -2,6 +2,19 @@
 </script>
 
 <template>
+  <div class="form-container">
+    <div class="input-container">
+      <div>
+        <label>name:</label>
+        <input class="input" type="text">
+      </div>
+      <div>
+        <label>age:</label>
+        <input class="input" type="text">
+      </div>
+    </div>
+    <button class="register-button">Post</button>
+  </div>
 </template>
 
 <style scoped>
