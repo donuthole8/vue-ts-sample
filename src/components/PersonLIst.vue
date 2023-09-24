bf022cd627225a2cbb29fc84aae78a8161cddc69
donuthole8 <muro5866@gmail.com>
2023-09-24 20:11:51 +0900

    feat:確認アラート

--- src/components/PersonLIst.vue
+++ src/components/PersonLIst.vue
@@ -9,8 +9,10 @@ defineProps<Props>()
 
 const emit = defineEmits(['delete'])
 
-const onClickDelete = (id: number) => {
-  emit('delete', id)
+const onClickDelete = (id: number, name: string) => {
+  if (confirm('Delete ' + name + '?')) {
+    emit('delete', id)
+  }
 }
 </script>
 
@@ -18,7 +20,7 @@ const onClickDelete = (id: number) => {
   <li class="flex flex-row justify-center items-center bg-teal-500 rounded m-4" v-for="person in persons" :key="person.id">
     <span class="basis-1/2 m-2">{{ person.name }}</span>
     <span class="basis-1/4 m-2">{{ person.age }}</span>
-    <button class="basis-1/4 m-2" @click="onClickDelete(person.id)">
+    <button class="basis-1/4 m-2" @click="onClickDelete(person.id, person.name)">
       <span>delete</span>
     </button>
   </li>
